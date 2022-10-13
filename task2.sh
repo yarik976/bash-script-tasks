ea9df05edf154ff391b23f421091ae791dd72e4d
yarik976 <yarik97.6@gmail.com>
2022-10-13 17:23:56 +0000

    first commit

---
+++ task2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$#"  == 1 ]; then
+	echo $1
+elif [ "$#"  == 2 ]; then
+	echo -e "$1\n$2"
+elif [ "$#"  == 3 ]; then
+	echo -e "$1\n$2\n$3"
+else
+	echo "There no arguments or more than 3"
+fi
